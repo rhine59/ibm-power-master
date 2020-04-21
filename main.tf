9a71738e6317739150d715c25617ac7e8ac01f73
rhine59 <rhine59@gmail.com>
2020-04-21 20:00:27 +0100

    apr21

--- main.tf
+++ main.tf
@@ -14,7 +14,7 @@ resource "ibm_pi_network" "power_networks" {
   count                = 1
   pi_network_name      = var.networkname
   pi_cloud_instance_id = var.powerinstanceid
-  pi_network_type      = "public-vlan"
+  pi_network_type      = "pub-vlan"
 }
 
 data "ibm_pi_public_network" "public" {
